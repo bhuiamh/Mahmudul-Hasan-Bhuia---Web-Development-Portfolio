79f7451874fb74a110983196f2352b0ece7ce0f6
bhuiamhe <bhuiamh@gmail.com>
2023-07-03 00:23:00 +0600

    half of about

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Works,
+  StarsCanvas,
+  Navbar,
+  Tech,
+} from "./components";
 const App = () => {
-  return <div>WEB Developer Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar></Navbar>
+          <Hero></Hero>
+        </div>
+        <About></About>
+        <Experience></Experience>
+        <Tech></Tech>
+        <Works></Works>
+        <Feedbacks></Feedbacks>
+        <div className="relative z-0">
+          <Contact></Contact>
+          <StarsCanvas></StarsCanvas>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] "
+      >
+        I'm a skilled MERN full-stack web developer with expertise in
+        TypeScript, JavaScript, and frameworks like React and Node.js. I excel
+        at collaborating closely with clients to create efficient, scalable, and
+        user-friendly solutions that solve real-world problems. Let's bring your
+        ideas to life together!
+      </motion.p>
+    </>
+  );
+};
 
-export default About
+export default About;
